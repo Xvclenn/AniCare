60d3f7921f7ef48b120b49828605fab1b35ca738
Xvclenn <https://github.com/Xvclenn>
2023-11-21 02:11:25 +0800

    navbar and header finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import style from "./page.module.css";
 
 export default function Home() {
-	return (
-		<div className={style.container}>
-			Home
-			<h1 className="">Hello world!</h1>
-		</div>
-	);
+	return <div className={style.container}>Home</div>;
 }
